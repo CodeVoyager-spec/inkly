aeb689e22e3f933213c1ce06b5eb23976bdb8f2c
Raj Narayan <rajnarayankumar139@gmail.com>
2026-01-17 09:01:49 +0000

    fix bugs and improve code

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+const User = require("./models/user.model");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Welcome to cms API"));
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to CMS API 🚀");
 });
+
+// Start server after DB connection
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+startServer();
