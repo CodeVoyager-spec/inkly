430668ac65eb94b8b02f3472a237d35df98aab87
Raj Narayan <raj@ASUS.localdomain>
2026-01-17 08:03:59 +0000

    initial commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Welcome to cms API"))
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
